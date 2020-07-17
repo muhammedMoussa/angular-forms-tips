7b1a394ab628ba76b1adff949239c7f75b20ac5c
Muhammed <muhammedmoussa@hotmail.com>
2020-07-17 13:37:07 +0200

    update: shell css utils

--- src/app/tips-shell/tips-shell.component.ts
+++ src/app/tips-shell/tips-shell.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tips-shell',
-  template: `
-    <app-functional-form></app-functional-form>
-    <app-observable-form></app-observable-form>
-  `,
+  templateUrl: './tips-shell.component.html',
   styles: [
   ]
 })
